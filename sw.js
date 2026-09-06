ebf6764eeb09748fcafddca51667c118ae5a7635
Teresa Campbell <teresa.j.campbell@gmail.com>
2026-09-06 14:30:56 -0400

    Expand monthly hero missions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'debt-ledger-v3';
+const CACHE = 'debt-ledger-v4';
 const FILES = ['./index.html', './manifest.webmanifest', './ledger-icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
